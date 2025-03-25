8181d02ac2dba5a1906b414bfc5342aa6804fa28
inyourtime <inyourtimeguy@gmail.com>
2025-03-25 14:56:43 +0700

    chore: use createMiddleware

--- src/middlewares/verifyLineSignature.ts
+++ src/middlewares/verifyLineSignature.ts
@@ -1,27 +1,26 @@
 import crypto from "node:crypto";
-import { Context, Next } from "hono";
 import { Bindings } from "../types";
+import { createMiddleware } from "hono/factory";
 
-export async function verifyLineSignature(
-  c: Context<{ Bindings: Bindings }>,
-  next: Next
-) {
-  const signature = c.req.header("x-line-signature");
-  if (!signature) {
-    console.error("Missing signature header!");
-    return c.text("Unauthorized", 401);
+export const verifyLineSignature = createMiddleware<{ Bindings: Bindings }>(
+  async (c, next) => {
+    const signature = c.req.header("x-line-signature");
+    if (!signature) {
+      console.error("Missing signature header!");
+      return c.text("Unauthorized", 401);
+    }
+
+    const rawBody = await c.req.raw.clone().bytes();
+    const computedSignature = crypto
+      .createHmac("SHA256", c.env.LINE_CHANNEL_SECRET)
+      .update(rawBody)
+      .digest("base64");
+
+    if (signature !== computedSignature) {
+      console.error("Signature verification failed!");
+      return c.text("Unauthorized", 401);
+    }
+
+    return next();
   }
-
-  const rawBody = await c.req.raw.clone().bytes();
-  const computedSignature = crypto
-    .createHmac("SHA256", c.env.LINE_CHANNEL_SECRET)
-    .update(rawBody)
-    .digest("base64");
-
-  if (signature !== computedSignature) {
-    console.error("Signature verification failed!");
-    return c.text("Unauthorized", 401);
-  }
-
-  return next();
-}
+);
